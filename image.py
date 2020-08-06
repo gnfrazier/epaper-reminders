3e1742935221cec831e2cc07fee085c928dcb77e
greg <gnfrazier@gmail.com>
2020-08-05 21:59:19 -0400

    Update font handling. Adjust bounding rectangle.

--- image.py
+++ image.py
@@ -1,15 +1,23 @@
 
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+
+
 from PIL import Image,ImageDraw,ImageFont
 
 try: # this is for quick developement only
     import epd2in7b
     raspi = True
     
+    font_path = '/usr/share/fonts/opentype/linux-libertine/LinBiolinum_R.otf'
+
+    
 except:
     
     import matplotlib.pyplot as plt
     raspi = False
-    
+    font_path = "/usr/share/fonts/truetype/clear-sans/ClearSans-Regular.ttf"
     
     # The folliwing line is useful in Jupyter notebook
     # %matplotlib inline
@@ -22,48 +30,41 @@ if raspi:
     epd.init()
     epd.Clear()
 
-    blackimage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    redimage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-    HBlackimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126
-    HRedimage = Image.new('1', (epd.height, epd.width), 255)  # 298*126    
+    # blackimage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    # redimage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
+    h_black_image = Image.new('1', (epd.height, epd.width), 255)  # 298*126
+    h_red_image = Image.new('1', (epd.height, epd.width), 255)  # 298*126    
  
     
 else:
-    blackimage = Image.new('1', (298, 126), 255)  # 255: clear the frame
-    redimage = Image.new('1', (298, 126), 255)  # 255: clear the frame
-    HBlackimage = Image.new('1', (264, 126), 255)  # 298*126
-    HRedimage = Image.new('1', (264, 126), 255)  # 298*126    
+    # blackimage = Image.new('1', (298, 126), 255)  # 255: clear the frame
+    # redimage = Image.new('1', (298, 126), 255)  # 255: clear the frame
+    h_black_image = Image.new('1', (298, 126), 255)  # 298*126
+    h_red_image = Image.new('1', (298, 126), 255)  # 298*126    
 
 
 
-font_path = "usr/share/fonts/truetype/"
-try:
-    font24 = ImageFont.truetype('usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf', 24) #24 Chars across the screen
-    font18 = ImageFont.truetype('usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf', 18)
-except:
-    font24 = ImageFont.load_default()
-    font18 = ImageFont.load_default()
 
-drawblack = ImageDraw.Draw(HBlackimage)
-drawred = ImageDraw.Draw(HRedimage)
+
+font24 = ImageFont.truetype(font_path, 24) #24 Chars across the screen
+font18 = ImageFont.truetype(font_path, 18)
+
+#    font24 = ImageFont.load_default()
+#    font18 = ImageFont.load_default()
+
+drawblack = ImageDraw.Draw(h_black_image)
+drawred = ImageDraw.Draw(h_red_image)
 drawblack.text((10, 0), 'hello world', font = font24, fill = 0)
 drawblack.text((10, 30), 'abcdefghijklmnopqrstu', font = font24, fill = 0)
 drawblack.text((10, 60), 'abcdefghijklmnopqrstu', font = font24, fill = 0)
 drawblack.text((10, 90), 'abcdefghijklmnopqrstu', font = font24, fill = 0)
 
-#drawblack.text((150, 0), 'another line', font = font24, fill = 0)    
-#drawblack.line((20, 50, 70, 100), fill = 0)
-#drawblack.line((70, 50, 20, 100), fill = 0)
-#drawblack.rectangle((20, 50, 70, 100), outline = 0)    
-drawred.line((165, 50, 165, 100), fill = 0)
-drawred.line((140, 75, 190, 75), fill = 0)
-drawred.arc((140, 50, 190, 100), 0, 360, fill = 0)
-drawred.rectangle((80, 50, 130, 100), fill = 0)
+
 drawblack.rectangle((0, 0, 264, 126), outline = 0, width= 2)    
 
 # plt.imshow(drawblack)
 if raspi:
-    epd.display(epd.getbuffer(HBlackimage), epd.getbuffer(HRedimage))
+    epd.display(epd.getbuffer(h_black_image), epd.getbuffer(h_red_image))
 
 else:
-    display(HBlackimage)
+    display(h_black_image)
