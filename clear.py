5cbb21e0b614c2b2f3942029bd4962927b2bd6ce
greg <gnfrazier@gmail.com>
2020-08-05 23:11:15 -0400

    Add clear.py

---
+++ clear.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+from PIL import Image,ImageDraw,ImageFont
+
+import epd2in7b
+
+if name = __main__:
+    epd = epd2in7b.EPD()
+
+    epd.init()
+    epd.Clear()
